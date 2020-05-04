41ce2644f13ffc2a407c91914afa8fb2b92d887a
Kirill Morozov <kirill@robotix.pro>
2020-05-04 21:50:22 +0300

    Add .zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,6 @@ ln -s "$(pwd)/.vimrc" ~/.vimrc
 ln -s "$(pwd)/vim" ~/.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Zsh
+ln -s "$(pwd)/.zshrc" ~/.zshrc
