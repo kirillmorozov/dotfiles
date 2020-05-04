9eeb1adc2fa6e8d9a83015672fc2d9994e5cd54d
Kirill Morozov <kirill@robotix.pro>
2020-05-04 21:53:35 +0300

    Fix Vim Plug installation command

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 # Vim
 ln -s "$(pwd)/.vimrc" ~/.vimrc
 ln -s "$(pwd)/vim" ~/.vim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Zsh
 ln -s "$(pwd)/.zshrc" ~/.zshrc
