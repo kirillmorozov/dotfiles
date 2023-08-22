352aec9d20a866afe6906b0558ebc7e56f1ee14e
Kirill Morozov <kirill.morozov@native-robotics.com>
2023-08-22 17:57:11 +0300

    feat(wezterm): automatically switch light/dark theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,8 +10,22 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
 
--- For example, changing the color scheme:
-config.color_scheme = 'Solarized Dark (Gogh)'
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Builtin Solarized Dark'
+  else
+    return 'Builtin Solarized Light'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 return config
