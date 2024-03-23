6041a98ac4c9f9fc78ee915fc8f681df6ecd8a62
Kirill Morozov <kirill@robotix.pro>
2024-03-23 19:56:27 +0200

    chore(neovim): remove unused comments

--- neovim/init.lua
+++ neovim/init.lua
@@ -327,15 +327,6 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			-- Enable the following language servers Feel free to add/remove any LSPs
-			-- that you want here. They will automatically be installed.
-			--
-			--  Add any additional override configuration in the following tables. Available keys are:
-			--  - cmd (table): Override the default command used to start the server
-			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
-			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				bashls = {},
 				clangd = {},
@@ -352,13 +343,6 @@ require("lazy").setup({
 				pylsp = {},
 				terraformls = {},
 			}
-
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-			})
 		end,
 	},
 
