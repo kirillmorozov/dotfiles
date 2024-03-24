8b545295d608b124f310fc99d23d43166fae32ae
Kirill Morozov <kirill@robotix.pro>
2024-03-24 11:14:04 +0200

    feat(neovim): configure lua_ls

--- neovim/init.lua
+++ neovim/init.lua
@@ -386,11 +386,26 @@ require("lazy").setup({
 				clangd = {},
 				gopls = {},
 				lua_ls = {
+					on_init = function(client)
+						local path = client.workspace_folders[1].name
+						if
+							vim.loop.fs_stat(path .. "/.luarc.json")
+							or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+						then
+							return
+						end
+						client.config.settings.Lua =
+							vim.tbl_deep_extend("force", client.config.settings.Lua, {
+								runtime = { version = "LuaJIT" },
+								workspace = {
+									checkThirdParty = false,
+									library = { vim.env.VIMRUNTIME },
+								},
+							})
+					end,
 					settings = {
 						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
+							completion = { callSnippet = "Replace" },
 						},
 					},
 				},
