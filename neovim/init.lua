47165e816ec19b35461da4edd458c31ac2932bbe
Kirill Morozov <kirill@robotix.pro>
2024-03-24 11:36:18 +0200

    fix(neovim): selection inside brackets

--- neovim/init.lua
+++ neovim/init.lua
@@ -588,11 +588,6 @@ require("lazy").setup({
 	{
 		"echasnovski/mini.nvim",
 		config = function()
-			-- Better Around/Inside textobjects
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 			-- - sd'   - [S]urround [D]elete [']quotes
