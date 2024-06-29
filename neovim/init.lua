783e0cc64d400e72b3739e138e464ebd7370785e
Kirill Morozov <kirill@robotix.pro>
2024-06-29 16:00:51 +0300

    feat(neovim): add lazygit plugin

--- neovim/init.lua
+++ neovim/init.lua
@@ -171,7 +171,25 @@ require("lazy").setup({
 			},
 		},
 	},
-
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<C-G>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 	-- Useful plugin to show you pending keybinds.
 	{
 		"folke/which-key.nvim",
@@ -597,14 +615,14 @@ require("lazy").setup({
 		tag = "v1.2.0",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { 
+		opts = {
 			signs = false,
 			highlight = {
 				pattern = [[(KEYWORDS)\s*(\([^\)]*\))?:]],
 			},
 			search = {
 				pattern = [[\b(KEYWORDS)(?:\(\w+\))?:]],
-			}
+			},
 		},
 	},
 
