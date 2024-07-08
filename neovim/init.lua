393677f6a1847fb4177225f80d5213b117e53083
Kirill Morozov <kirill@robotix.pro>
2024-07-08 22:17:54 +0300

    feat(neovim): add gitlab ci schema

--- neovim/init.lua
+++ neovim/init.lua
@@ -456,6 +456,7 @@ require("lazy").setup({
 					settings = {
 						yaml = {
 							schemas = {
+								["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "/.gitlab-ci.yml",
 								["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
 							},
 						},
