824384cd2d967b5736f27262f55b054951760100
Kirill Morozov <kirill@robotix.pro>
2024-07-08 21:24:25 +0300

    feat: use yq as YAML formatter (#61)

--- neovim/init.lua
+++ neovim/init.lua
@@ -498,14 +498,12 @@ require("lazy").setup({
 				},
 			},
 			formatters_by_ft = {
-				go = {
-					{ "gofumpt", "gofmt" },
-				},
+				go = { { "gofumpt", "gofmt" } },
 				lua = { "stylua" },
 				python = { "ruff_format" },
 				sh = { "shfmt" },
 				terraform = { "terraform_fmt" },
-				yaml = { "yamlfmt" },
+				yaml = { { "yamlfmt", "yq" } },
 				json = { "jq" },
 				["_"] = { "trim_whitespace" },
 			},
