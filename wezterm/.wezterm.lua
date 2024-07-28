2697fb8734ebee9c173f98955044598ef57f38e6
Kirill Morozov <kirill@robotix.pro>
2024-07-28 12:15:23 +0300

    feat(wezterm): update configuration (#69)
    Remove window padding, hide scroll bar and tab bar if there's only one
    tab and set font to the Nerd Font patched Fira Code with a fallback to
    the regular Fira Code.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,14 +1,5 @@
 local wezterm = require 'wezterm'
 
---- This table will hold the configuration.
-local config = {}
-
--- NOTE(kirillmorozov): In newer versions of wezterm, use the config_builder
--- which will help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
 --- Returns the appearance of the window environment.
 -- If gui is not available returns 'Dark'.
 local function get_appearance()
@@ -30,12 +21,25 @@ local function scheme_for_appearance(appearance)
   end
 end
 
+local config = {}
+-- NOTE(kirillmorozov): In newer versions of wezterm, use the config_builder
+-- which will help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.enable_scroll_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
 config.color_scheme = scheme_for_appearance(get_appearance())
 
-config.font = wezterm.font_with_fallback {
-  { family = 'Fira Code', weight = 'Regular' },
-  { family = 'Cascadia Code', weight = 'DemiLight' },
-}
+config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font', 'Fira Code' }
 config.font_size = 14.0
 
 return config
