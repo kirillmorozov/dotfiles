3228dd079ec6817915abb2a403a0abc985f1a133
Kirill Morozov <kirill@robotix.pro>
2024-09-01 22:14:05 +0300

    refactor(neovim): `init.lua`

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,25 +5,22 @@ vim.g.maplocalleader = " "
 -- Install `lazy.nvim` plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"--branch=stable",
-		lazyrepo,
+		"https://github.com/folke/lazy.nvim.git",
 		lazypath,
 	})
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+end
 
 -- Add lazy to the `runtimepath`, this allows us to `require` it.
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- Set up lazy, and load `lua/custom/plugins/` folder
-require("lazy").setup({ import = "custom/plugins" }, {
-	change_detection = {
-		notify = false,
-	},
-})
+require("lazy").setup(
+	{ import = "custom/plugins" },
+	{ change_detection = { notify = false } }
+)
