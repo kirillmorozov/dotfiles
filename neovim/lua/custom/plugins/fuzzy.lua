fe67bce52c0cffba1d4aa14044a26a38544111a5
Kirill Morozov <kirill@robotix.pro>
2024-09-20 10:59:51 +0300

    refactor(neovim): configuration (#88)
    Split one huge `init.lua` config file into several modules.

---
+++ neovim/lua/custom/plugins/fuzzy.lua
@@ -0,0 +1,115 @@
+-- Fuzzy Finder (files, lsp, etc)
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- If encountering errors, see telescope-fzf-native README for
+			-- installation instructions
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- `build` is used to run some command when the plugin is
+				-- installed/updated. This is only run then, not every time Neovim
+				-- starts up.
+				build = "make",
+				-- `cond` is a condition used to determine whether this plugin should
+				-- be installed and loaded.
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+			-- Useful for getting pretty icons, but requires a Nerd Font.
+			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		},
+		config = function()
+			-- Telescope is a fuzzy finder that comes with a lot of different things
+			-- that it can fuzzy find! It's more than just a "file finder", it can
+			-- search many different aspects of Neovim, your workspace, LSP, and
+			-- more!
+
+			-- The easiest way to use Telescope, is to start by doing something like:
+			-- :Telescope help_tags
+
+			-- After running this command, a window will open up and you're able to
+			-- type in the prompt window. You'll see a list of `help_tags` options
+			-- and a corresponding preview of the help.
+
+			-- Two important keymaps to use while in Telescope are:
+			--  - Insert mode: <c-/>
+			--  - Normal mode: ?
+
+			-- This opens a window that shows you all of the keymaps for the current
+			-- Telescope picker. This is really useful to discover what Telescope can
+			-- do as well as how to actually do it!
+
+			-- [[ Configure Telescope ]]
+			-- See `:help telescope` and `:help telescope.setup()`
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"^.git/",
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			-- Enable Telescope extensions if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			-- See `:help telescope.builtin`
+			local builtin = require("telescope.builtin")
+			vim.keymap.set(
+				"n",
+				"<leader>'",
+				builtin.resume,
+				{ desc = "Opens the previous picker" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>/",
+				builtin.live_grep,
+				{ desc = "Search for a string and get results live" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>?",
+				builtin.commands,
+				{ desc = "List available commands" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>b",
+				builtin.buffers,
+				{ desc = "List open buffers" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>d",
+				builtin.diagnostics,
+				{ desc = "List diagnostics" }
+			)
+			vim.keymap.set("n", "<leader>f", function()
+				builtin.find_files({ hidden = true })
+			end, { desc = "Search for files" })
+			vim.keymap.set(
+				"n",
+				"<leader>h",
+				builtin.help_tags,
+				{ desc = "List available help tags" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>j",
+				builtin.jumplist,
+				{ desc = "List items from Vim's jumplist" }
+			)
+		end,
+	},
+}
---
+++ neovim/lua/custom/plugins/tree-sitter.lua
@@ -0,0 +1,113 @@
+-- Highlight, edit, and navigate code
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"comment",
+				"diff",
+				"dockerfile",
+				"gitcommit",
+				"gleam",
+				"go",
+				"json",
+				"lua",
+				"markdown",
+				"python",
+				"terraform",
+				"toml",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["aa"] = {
+							query = "@parameter.outer",
+							desc = "Select around argument/parameter",
+						},
+						["ac"] = {
+							query = "@comment.outer",
+							desc = "Select around comment",
+						},
+						["af"] = {
+							query = "@function.outer",
+							desc = "Select around function",
+						},
+						["at"] = {
+							query = "@class.outer",
+							desc = "Select around type/class",
+						},
+						["ia"] = {
+							query = "@parameter.inner",
+							desc = "Select inside argument/parameter",
+						},
+						["ic"] = {
+							query = "@comment.inner",
+							desc = "Select inside comment",
+						},
+						["if"] = {
+							query = "@function.inner",
+							desc = "Select inside function",
+						},
+						["it"] = {
+							query = "@class.inner",
+							desc = "Select inside type/class",
+						},
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]a"] = {
+							query = "@parameter.inner",
+							desc = "Next argument/parameter",
+						},
+						["]c"] = { query = "@comment.outer", desc = "Next comment" },
+						["]f"] = { query = "@function.outer", desc = "Next function" },
+						["]t"] = { query = "@class.outer", desc = "Next type/class" },
+					},
+					goto_previous_start = {
+						["[a"] = {
+							query = "@parameter.inner",
+							desc = "Previous argument/parameter",
+						},
+						["[c"] = { query = "@comment.outer", desc = "Previous comment" },
+						["[f"] = {
+							query = "@function.outer",
+							desc = "Previous function",
+						},
+						["[t"] = {
+							query = "@class.outer",
+							desc = "Previous type/class",
+						},
+					},
+				},
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as
+				-- Ruby) for indent rules. If you are experiencing weird indenting
+				-- issues, add the language to the list of
+				-- additional_vim_regex_highlighting and disabled languages for indent.
+				additional_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
+		},
+		config = function(_, opts)
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+}
