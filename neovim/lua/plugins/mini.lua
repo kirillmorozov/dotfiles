ed9eed92f171639a618dd9f4e97344e1d63534c6
Kirill Morozov <kirill@robotix.pro>
2024-10-12 20:36:27 +0300

    feat(neovim): replace gitsigns and todo-comments with mini

--- neovim/lua/plugins/mini.lua
+++ neovim/lua/plugins/mini.lua
@@ -2,6 +2,11 @@
 return {
 	{
 		"echasnovski/mini.nvim",
+		config = function(_, opts)
+			for plugin, options in pairs(opts) do
+				require("mini." .. plugin).setup(options)
+			end
+		end,
 		-- NOTE(kirillmorozov): opts is a table that holds config options for mini
 		-- modules.
 		opts = {
@@ -11,19 +16,16 @@ return {
 					comment_visual = "<C-c>",
 				},
 			},
+			diff = {},
+			git = {},
+			-- Simple and easy statusline. You could remove this setup call if you
+			-- don't like it, and try some other statusline plugin
+			statusline = { use_icons = true },
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 			-- - sd'   - [S]urround [D]elete [']quotes
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			surround = {},
-			-- Simple and easy statusline. You could remove this setup call if you
-			-- don't like it, and try some other statusline plugin
-			statusline = { use_icons = true },
 		},
-		config = function(_, opts)
-			for plugin, options in pairs(opts) do
-				require("mini." .. plugin).setup(options)
-			end
-		end,
 	},
 }
