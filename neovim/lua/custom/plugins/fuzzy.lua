fc2b2d4e8e631fa9a7d354fbc7cf33ee3c1875cc
Kirill Morozov <kirill@robotix.pro>
2024-10-12 08:30:56 +0300

    feat(neovim): load `fzf-lua` very lazy

--- neovim/lua/custom/plugins/fuzzy.lua
+++ neovim/lua/custom/plugins/fuzzy.lua
@@ -2,7 +2,7 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
-		event = "VimEnter",
+		event = "VeryLazy",
 		-- optional for icon support
 		dependencies = { "echasnovski/mini.nvim", version = "*" },
 		config = function()
