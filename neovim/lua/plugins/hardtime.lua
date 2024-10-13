21ff35d964beca758e7ec53495cd61f7470791ec
Kirill Morozov <kirill@robotix.pro>
2024-10-13 21:24:43 +0300

    chore(neovim): reformat table

--- neovim/lua/plugins/hardtime.lua
+++ neovim/lua/plugins/hardtime.lua
@@ -2,10 +2,7 @@
 return {
 	{
 		"m4xshen/hardtime.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-		},
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 		event = "VeryLazy",
 		opts = {},
 	},
