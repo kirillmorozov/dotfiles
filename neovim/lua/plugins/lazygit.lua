1b6284d572502d81370d698073e9564a07a9d914
Kirill Morozov <kirill@robotix.pro>
2024-10-14 23:16:57 +0300

    style(neovim): reformat `lazygit.lua`

--- neovim/lua/plugins/lazygit.lua
+++ neovim/lua/plugins/lazygit.lua
@@ -8,14 +8,7 @@ return {
 			"LazyGitFilter",
 			"LazyGitFilterCurrentFile",
 		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<C-G>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = { { "<C-G>", "<cmd>LazyGit<cr>", desc = "LazyGit" } },
 	},
 }
