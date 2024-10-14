35683c1e999afa80a445021302220aa0f408094b
Kirill Morozov <kirill@robotix.pro>
2024-10-14 22:42:25 +0300

    style(neovim): reformat lsp config

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -4,17 +4,12 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			-- Useful status updates for LSP.
-			{
-				"j-hui/fidget.nvim",
-				opts = {},
-			},
+			{ "j-hui/fidget.nvim" },
 			-- `neodev` configures Lua LSP for your Neovim config, runtime and
 			-- plugins used for completion, annotations and signatures of Neovim apis
 			{
 				"folke/neodev.nvim",
-				opts = {
-					root_dir = vim.fn.getcwd(),
-				},
+				opts = { root_dir = vim.fn.getcwd() },
 			},
 		},
 		event = "VeryLazy",
