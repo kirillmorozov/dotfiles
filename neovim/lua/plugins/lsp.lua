caeaf9cfcd52a28f086f7f56975ee3f2835624ee
Kirill Morozov <kirill@robotix.pro>
2024-12-07 19:19:28 +0100

    feat(neovim): replace `neodev.nvim` with `lazydev.nvim`

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -5,11 +5,16 @@ return {
 		dependencies = {
 			-- Useful status updates for LSP.
 			{ "j-hui/fidget.nvim" },
-			-- `neodev` configures Lua LSP for your Neovim config, runtime and
-			-- plugins used for completion, annotations and signatures of Neovim apis
 			{
-				"folke/neodev.nvim",
-				opts = { root_dir = vim.fn.getcwd() },
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details Load luvit types
+						-- when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
 			},
 		},
 		event = { "BufNew", "BufRead" },
