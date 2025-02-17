51c1bb7bc4806130c80567e2c1a6848db7dde14f
Kirill Morozov <kirill@robotix.pro>
2025-02-17 20:52:24 +0100

    build: bump blink version to 0.12.4

--- neovim/lua/plugins/blink.lua
+++ neovim/lua/plugins/blink.lua
@@ -3,15 +3,17 @@ return {
 	"saghen/blink.cmp",
 	dependencies = "rafamadriz/friendly-snippets",
 	event = "InsertEnter",
-	version = "v0.11.0",
+	version = "v0.12.4",
 	opts_extend = { "sources.default" },
 	opts = {
 		keymap = {
 			preset = "default",
 		},
+		cmdline = {
+			sources = {},
+		},
 		sources = {
 			default = { "buffer", "lsp", "snippets", "path" },
-			cmdline = {},
 		},
 	},
 }
