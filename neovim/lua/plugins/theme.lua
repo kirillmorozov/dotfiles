108081a824d791843e0f217165ec146f7a1fe1c2
Kirill Morozov <kirill@robotix.pro>
2025-03-21 21:41:52 +0100

    feat(neovim): disable italics (#120)

--- neovim/lua/plugins/theme.lua
+++ neovim/lua/plugins/theme.lua
@@ -4,9 +4,14 @@ return {
 		name = "rose-pine",
 		-- Make sure to load this before all the other start plugins.
 		priority = 1000,
+		opts = {
+			styles = {
+				italic = false,
+				transparency = false,
+			},
+		},
 		init = function()
 			vim.cmd.colorscheme("rose-pine")
-			vim.cmd.highlight("Comment gui=none")
 		end,
 	},
 }
