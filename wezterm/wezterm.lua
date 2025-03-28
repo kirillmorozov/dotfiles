3b375b2b8422fd417dc407eba8709abe2e55d3bf
Kirill Morozov <kirill@robotix.pro>
2025-03-28 20:32:32 +0100

    refactor: plugin configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -31,7 +31,6 @@ end
 local color_scheme_name = scheme_for_appearance(get_appearance())
 local color_scheme = wezterm.color.get_builtin_schemes()[color_scheme_name]
 local font = wezterm.font_with_fallback({ "FiraCode Nerd Font", "Fira Code" })
-local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 
 config.animation_fps = 120
 config.color_scheme = color_scheme_name
@@ -41,24 +40,26 @@ config.font_size = 14.0
 config.max_fps = 120
 config.window_padding = { bottom = 0, left = 0, right = 0, top = 0 }
 
-bar.apply_to_config(config, {
-	separator = {
-		field_icon = "",
-		left_icon = "❯",
-		right_icon = "",
-		space = 1,
-	},
-	modules = {
-		clock = { enabled = false },
-		cwd = { enabled = false },
-		hostname = { enabled = false },
-		leader = { enabled = false },
-		pane = { enabled = false },
-		spotify = { enabled = false },
-		tabs = { active_tab_fg = 5, inactive_tab_fg = 8 },
-		username = { enabled = false },
-		workspace = { enabled = false },
-	},
-})
+wezterm.plugin
+	.require("https://github.com/adriankarlen/bar.wezterm")
+	.apply_to_config(config, {
+		separator = {
+			field_icon = "",
+			left_icon = "❯",
+			right_icon = "",
+			space = 1,
+		},
+		modules = {
+			clock = { enabled = false },
+			cwd = { enabled = false },
+			hostname = { enabled = false },
+			leader = { enabled = false },
+			pane = { enabled = false },
+			spotify = { enabled = false },
+			tabs = { active_tab_fg = 5, inactive_tab_fg = 8 },
+			username = { enabled = false },
+			workspace = { enabled = false },
+		},
+	})
 
 return config
