857787e79593b9116372b13f5ded29ba5526d0e9
Kirill Morozov <kirill@robotix.pro>
2025-05-07 21:56:29 +0200

    feat: enable blink but disable autotrigger

--- neovim/lua/plugins/blink.lua
+++ neovim/lua/plugins/blink.lua
@@ -2,19 +2,21 @@
 return {
 	"saghen/blink.cmp",
 	dependencies = "rafamadriz/friendly-snippets",
-	enabled = false,
 	event = "InsertEnter",
 	opts_extend = { "sources.default" },
 	version = "v1.1.1",
 	opts = {
+		cmdline = { enabled = false },
+		completion = {
+			trigger = {
+				show_on_keyword = false,
+				show_on_trigger_character = false,
+			},
+		},
 		keymap = {
 			preset = "default",
-		},
-		cmdline = {
-			enabled = false,
-		},
-		sources = {
-			default = { "buffer", "lsp", "path", "snippets" },
+			["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
+			["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
 		},
 	},
 }
