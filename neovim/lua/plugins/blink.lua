999c7d10ed82f7ff97d2a4b084f120d5a80c8837
Kirill Morozov <kirill@robotix.pro>
2025-05-08 11:51:22 +0200

    feat: trigger blink on regular maps

--- neovim/lua/plugins/blink.lua
+++ neovim/lua/plugins/blink.lua
@@ -3,17 +3,22 @@ return {
 	"saghen/blink.cmp",
 	dependencies = "rafamadriz/friendly-snippets",
 	event = "InsertEnter",
-	version = "v1.0.0",
 	opts_extend = { "sources.default" },
+	version = "v1.0.0",
 	opts = {
 		keymap = {
 			preset = "default",
+			["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
+			["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
+		},
+		completion = {
+			trigger = {
+				show_on_keyword = false,
+				show_on_trigger_character = false,
+			},
 		},
 		cmdline = {
 			enabled = false,
-		},
-		sources = {
-			default = { "buffer", "lsp", "path", "snippets" },
 		},
 	},
 }
