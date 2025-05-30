b58511e75181e9a9de9c5d1a98f3c10cd1519266
Kirill Morozov <kirill@robotix.pro>
2025-05-30 22:08:28 +0200

    style: split long line

--- neovim/lsp/ty.lua
+++ neovim/lsp/ty.lua
@@ -1,5 +1,10 @@
 return {
 	cmd = { "ty", "server" },
 	filetypes = { "python" },
-	root_markers = { ".git", ".ruff.toml" "pyproject.toml", "ruff.toml" },
+	root_markers = {
+		".git",
+		".ruff.toml",
+		"pyproject.toml",
+		"ruff.toml",
+	},
 }
