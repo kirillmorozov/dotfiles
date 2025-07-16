5e89301c41178066069c0a5ea052749b394478f2
Kirill Morozov <kirill@robotix.pro>
2025-07-16 21:16:57 +0200

    feat(neovim): move lines

--- neovim/plugin/keymaps.lua
+++ neovim/plugin/keymaps.lua
@@ -58,3 +58,14 @@ vim.keymap.set(
 	vim.lsp.buf.code_action,
 	{ desc = "Select a code action" }
 )
+
+-- Move text up and down
+vim.keymap.set("n", "<A-j>", "<Cmd>m<Space>.+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", "<Cmd>m<Space>.-2<CR>==", { desc = "Move line up" })
+vim.keymap.set(
+	"v",
+	"<A-j>",
+	"<Cmd>m<Space>'>+1<CR>gv",
+	{ desc = "Move selection down" }
+)
+vim.keymap.set("v", "<A-k>", "<Cmd>m<Space>'<-2<CR>gv", { desc = "Move selection up" })
