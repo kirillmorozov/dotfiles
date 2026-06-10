1163e4341d145a4c668939adf4a7277660daf4ec
Kirill Morozov <kirill@robotix.pro>
2026-06-10 22:25:28 +0200

    refactor: use abbreviations instead of aliases

--- fish/config.fish
+++ fish/config.fish
@@ -29,9 +29,10 @@ if command -q vimx
     alias vim vimx
 end
 
-alias e "eza -l --total-size --time-style long-iso --group-directories-first --icons"
-alias ea "e -a"
-alias et "e -T"
-alias lg lazygit
+set -l __eza_base "eza -l --total-size --time-style long-iso --group-directories-first --icons"
+abbr -a e $__eza_base
+abbr -a ea "$__eza_base -a"
+abbr -a et "$__eza_base -T"
+abbr -a lg lazygit
 
 starship init fish | source
