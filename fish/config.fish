c86564d64332aea52eec3a84041c9b878c288d93
Kirill Morozov <kirill@robotix.pro>
2026-06-11 09:07:41 +0200

    feat: vendor git abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Add user-specific executables to PATH.
-fish_add_path --prepend "/opt/homebrew/bin" "$HOME/.local/bin" "$HOME/go/bin" /usr/local/go/bin
+fish_add_path --prepend /opt/homebrew/bin "$HOME/.local/bin" "$HOME/go/bin" /usr/local/go/bin
 
 if status is-interactive
     set -gx BAT_THEME base16
@@ -35,18 +35,6 @@ if status is-interactive
     abbr -a e $__eza_base
     abbr -a ea "$__eza_base -a"
     abbr -a et "$__eza_base -T"
-    abbr -a g "git"
-    abbr -a ga "git add"
-    abbr -a gco "git commit --verbose"
-    abbr -a gd "git diff"
-    abbr -a gf "git fetch"
-    abbr -a gl "git pull"
-    abbr -a glgp "git log --stat --patch"
-    abbr -a gp "git push"
-    abbr -a gpf "git push --force-with-lease --force-if-includes"
-    abbr -a gpr "git pull --rebase"
-    abbr -a gst "git status"
-    abbr -a gsw "git switch"
     abbr -a lg lazygit
 
     starship init fish | source
