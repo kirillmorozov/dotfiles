fde9cc9ce53ce1b94ec1cc4c8ba58c3be87bc790
Kirill Morozov <kirill@robotix.pro>
2026-09-03 23:05:55 +0200

    feat(fish): abbreviation descriptions

--- fish/conf.d/dnf.fish
+++ fish/conf.d/dnf.fish
@@ -3,11 +3,11 @@
 # Oh-My-Zsh dnf plugin abbreviations.
 # Source: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/dnf/dnf.plugin.zsh
 
-abbr -a dnfc -- "sudo dnf clean all"
-abbr -a dnfi -- "sudo dnf install"
-abbr -a dnfl -- "dnf list"
-abbr -a dnfli -- "dnf list installed"
-abbr -a dnfp -- "dnf info"
-abbr -a dnfr -- "sudo dnf remove"
-abbr -a dnfs -- "dnf search"
-abbr -a dnfu -- "sudo dnf upgrade"
+abbr -a dnfc --description='clean all DNF caches' -- "sudo dnf clean all"
+abbr -a dnfi --description='install DNF packages' -- "sudo dnf install"
+abbr -a dnfl --description='list DNF packages' -- "dnf list"
+abbr -a dnfli --description='list installed DNF packages' -- "dnf list installed"
+abbr -a dnfp --description='show DNF package information' -- "dnf info"
+abbr -a dnfr --description='remove DNF packages' -- "sudo dnf remove"
+abbr -a dnfs --description='search DNF packages' -- "dnf search"
+abbr -a dnfu --description='upgrade DNF packages' -- "sudo dnf upgrade"
